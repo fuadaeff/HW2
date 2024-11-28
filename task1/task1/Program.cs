4fa344c1585a1b6237219564e735b01a2132934c
fuadaeff <fabdulla057@gmail.com>
2024-11-28 16:33:28 +0400

    Add files via upload

---
+++ task1/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter age:");
+            int age= int.Parse(Console.ReadLine());
+            if (age >=0 && age <= 12{ 
+                Console.WriteLine("Child");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else if (age >= 20 && age <= 64)
+            {
+                Console.WriteLine("Adult");
+            }
+            else
+            {
+                Console.WriteLine("Yasli");
+            }
+        }
+    }
+}
