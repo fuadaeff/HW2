1d604a1276690e4b34a40be9e39663eafc4b1d4d
fuadaeff <fabdulla057@gmail.com>
2024-11-28 16:34:04 +0400

    Add files via upload

---
+++ task2/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            int thirdNumber = int.Parse(Console.ReadLine());
+            if (firstNumber > secondNumber && firstNumber > thirdNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else if (secondNumber > thirdNumber && secondNumber > firstNumber)
+            {
+                Console.WriteLine(secondNumber);
+            }
+            else if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+            {
+                Console.WriteLine(thirdNumber);
+            }
+        }
+    }
+}
